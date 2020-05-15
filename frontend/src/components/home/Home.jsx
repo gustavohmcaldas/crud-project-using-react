8d8111daa841e499f8c05a225b7e936a43ded3fb
Gustavo Caldas <gustavohmcaldas@gmail.com>
2020-05-15 00:40:14 -0700

    Adjusting Content Component

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../templates/Main'
+
+export default props =>
+    <Main icon="home" title="Home"
+        subtitle="Registration system using React.">
+        <div className='display-4'> Welcome!</div>
+        <hr />
+        <p className="mb-0">System to exemplify the construction of 
+            a register developed using React!</p>
+    </Main>
